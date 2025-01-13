2ddb8c5083a765a1b9b8259fc0ece45f9b75347a
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2025-01-13 12:43:21 -0500

    Ticket creation enhancements

--- clan-points-bot/join_clan_view.py
+++ clan-points-bot/join_clan_view.py
@@ -9,28 +9,38 @@ class JoinClanView(discord.ui.View):
     
     @discord.ui.button(label="✅ Agree & Request to Join", style=discord.ButtonStyle.primary, custom_id="join_clan")
     async def join_clan(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # Prevent ticket creation if their user is already in the applicants collection
         try:
-            # Prevent ticket creation if their user is already in the applicants collection
             existing_applicant: discord.channel = self.bot.applicants_collection.find_one({"discord_id": interaction.user.id})
             if existing_applicant:
-                await interaction.response.send_message(f"{interaction.user.mention} - You already have an **open application.** {interaction.guild.get_channel(existing_applicant['ticket_channel_id']).mention}", ephemeral=True)
+                await interaction.response.send_message(f"You already have an open application. Please complete it here: {interaction.guild.get_channel(existing_applicant['ticket_channel_id']).mention}", ephemeral=True)
                 return
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error preventing ticket creation: {e}")
             return
         
-        # Create channels for new applicants
-        await interaction.response.send_message(f"Join clan request for {interaction.user.display_name}", ephemeral=True)
-        channel: discord.CategoryChannel = interaction.guild.get_channel(int(os.getenv('NEW_MEMBER_REQUESTS_CATEGORY_ID')))
-        new_ticket: discord.channel = await channel.create_text_channel(name=f"{interaction.user.display_name}")
-        await new_ticket.edit(category=channel)
-        
-        # Add applicant to the applicants collection
-        applicant = Applicant(
+        # Create channel for new applicants
+        try:
+            channel: discord.CategoryChannel = interaction.guild.get_channel(int(os.getenv('NEW_MEMBER_REQUESTS_CATEGORY_ID')))
+            new_ticket: discord.channel = await channel.create_text_channel(name=f"{interaction.user.display_name}")
+            await new_ticket.edit(category=channel)
+            await interaction.response.send_message(f"Ticket created! Please apply here: {new_ticket.mention}", ephemeral=True)
+            await interaction.guild.get_channel(new_ticket.id).send(f"Welcome {interaction.user.mention}! Please share the following about yourself:\n* Meow\nAn admin will be in touch shortly!")
+        except Exception as e:
+            print(f"Error creating channel for new applicant: {e}")
+            return
+
+        # Add the applicant to the applicants collection
+        try:
+            applicant = Applicant(
             discord_id = interaction.user.id,
             ticket_channel_id = new_ticket.id,
             starter_points = 0)
-        self.bot.applicants_collection.insert_one(applicant.to_dict())
+            self.bot.applicants_collection.insert_one(applicant.to_dict())
+        except Exception as e:
+            print(f"Error adding applicant to the DB collection: {e}")
+            return
+        
 
         
     @discord.ui.button(label="🏆 Rank up request", style=discord.ButtonStyle.secondary, custom_id="rank_up_request")
---
+++ readme.md
@@ -0,0 +1,6 @@
+Local development
+
+1. Navigate to clan-points-bot
+2. Copy 'example.env' and create '.env'
+3. Fill in the values
+4. Run docker compose watch at the root. This will start the bot and create a local Mongo database
