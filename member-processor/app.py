0465a3611980c6d756ba29246f02e940b5ce78f1
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2025-01-29 21:29:23 -0500

    Updated processor to generate sheets in batches

--- member-processor/app.py
+++ member-processor/app.py
@@ -2,31 +2,48 @@ import os
 import sys
 import pika
 import json
+import time
 from dotenv import load_dotenv
 from services.google_sheet_service import GoogleSheetsService
 from models.clan_member import ClanMember
+from constants import constants
+
+def process_message(sheets_service, body):
+    try:
+        member: ClanMember = ClanMember.from_dict(json.loads(body.decode('utf-8')))
+        url = sheets_service.create_sheet(member)
+        print(f"Created sheet for {member.discord_display_name}: {url}")
+        return True
+    except Exception as e:
+        print(f"Error processing clan member: {e}")
+        return False
 
 def main():
     load_dotenv()
     sheets_service = GoogleSheetsService() 
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-    new_member_channel = connection.channel()
-    new_member_channel.queue_declare(queue='new_member')
+    channel = connection.channel()
+    channel.queue_declare(queue='new_member')
     
-    # Handles google sheet generation for new members
-    def callback(ch, method, properties, body):
-        print(f" [x] Received sheet generation request: {body}")
-        try:
-            member: ClanMember = ClanMember.from_dict(json.loads(body.decode('utf-8')))
-            url = sheets_service.create_sheet(member)
-            print(f"Created sheet for {member.discord_display_name}: {url}")
-        except Exception as e:
-            print(f"Error getting clan member from request: {e}")
+    print(' [*] Starting batch processor. To exit press CTRL+C')
+    
+    while True:
+        messages_processed = 0
         
+        # Process messages
+        while messages_processed < constants.BATCH_SIZE:
+            method_frame, header_frame, body = channel.basic_get(queue='new_member', auto_ack=True)
+            
+            # No more messages in queue
+            if not method_frame:
+                break
+                
+            print(f" [x] Processing message: {body}")
+            if process_message(sheets_service, body):
+                messages_processed += 1
         
-    new_member_channel.basic_consume(queue='new_member', auto_ack=True, on_message_callback=callback)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    new_member_channel.start_consuming()
+        print(f" [*] Processed {messages_processed} messages. Waiting for next interval...")
+        time.sleep(constants.WAIT_TIME_SECONDS)
     
 if __name__ == '__main__':
     try:
